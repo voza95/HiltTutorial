3066008b7b478dda7e2b65177d957434098288e6
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2021-03-25 22:45:43 +0530

    Error resolved

--- app/src/main/java/com/example/hilttutorial/MainModule.kt
+++ app/src/main/java/com/example/hilttutorial/MainModule.kt
@@ -3,25 +3,24 @@ package com.example.hilttutorial
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-import org.jetbrains.annotations.NotNull
+import dagger.hilt.android.internal.managers.ActivityComponentManager
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import javax.inject.Named
 import javax.inject.Singleton
+/*
 
 @Module
-@InstallIn(ActivityComponent::class)
-object AppModule {
+@InstallIn(Singleton::class)
+object MainModule {
 
-
-    //@Singleton
+    @Singleton
     @Provides
     @Named("String1")
     fun provideTestString1() = "This is dummy string we will inject"
 
-    //@NotNull
-    //@Singleton
+    @Singleton
     @Provides
     @Named("String2")
     fun provideTestString2() = "This is dummy string we will inject as well"
 
-}
+}*/
